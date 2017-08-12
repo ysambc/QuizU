6ec7a49b2816fd0d29929270c093d24c94d24d95
ysambc <ysambc@gmail.com>
2017-08-12 14:43:54 -0500

    Update multipq_init.php

--- multipq_init.php
+++ multipq_init.php
@@ -4,14 +4,26 @@ defined('ABSPATH') or die("Cannot access pages directly.");
 
 /**
  * Plugin Name: QuizU
- * Plugin URI:  https://developer.wordpress.org/plugins/quizu/
+ * Plugin URI: https://github.com/ysambc/QuizU
  * Description: Create flexible branched / chained logic quizzes
  * Author: Jimmy Albert Bent Cano
+ * Author URI: http://codebent.totalh.net
  * Copyright: Jimmy Albert Bent Cano
  * License: GPL3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  * Text Domain: quizuint
  * Version: 1.0
+ *
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
  */
 
 function multipq_init(){
